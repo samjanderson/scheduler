ab102ccbab5ba5541e0081a69768d5f2a3accd63
samjanderson <sam.andersonnn@live.ca>
2021-02-16 12:53:49 -0800

    complete confirm component

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 storiesOf("Button", module)
   .addParameters({
@@ -143,10 +144,17 @@ storiesOf("Appointment", module)
   .add("Show", () =>
     <Show
       student="Lydia Miller-Jones"
-      interviewer={{ interviewer }}
+      interviewer={ interviewer }
       onEdit={action("onEdit")}
       onDelete={action("onDelete")}
-    />);
+    />)
+  .add("Confirm", () =>
+    <Confirm
+      message="Delete the appointment?"
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")}
+    />)
+    
 
 
 
