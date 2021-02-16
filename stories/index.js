e4d72f32bdeed2ca392fbe5a4198153bfbe7b1fd
samjanderson <sam.andersonnn@live.ca>
2021-02-16 13:15:25 -0800

    complete more appointment views including error and status components

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,8 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -154,7 +156,12 @@ storiesOf("Appointment", module)
       onConfirm={action("onConfirm")}
       onCancel={action("onCancel")}
     />)
-    
+    .add("Status", () => <Status message="Deleting" />)
+    .add("Error", () =>
+    <Error
+      message="Could not delete appointment"
+      onClose={action("onClose")}
+    />);
 
 
 
