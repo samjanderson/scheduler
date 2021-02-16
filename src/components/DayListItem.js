4962d7b9912e572fa2091a05e28a2d408ea78028
samjanderson <sam.andersonnn@live.ca>
2021-02-15 17:15:51 -0800

    test DayListItem complete

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,10 +6,28 @@ import classNames from "classnames";
 export default function DayListItem(props) {
   //this is a string that will always be there, so its something you want to have at the end
   const dayClass = classNames("day-list__item", {
-    "day-list__item": true,
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0,
   });
+ 
+  const formatSpots = function () {
+    return props.spots === 0
+      ? "no spots remaining"
+      : props.spots === 1
+      ? "1 spot remaining"
+      : `${props.spots} spots remaining`;
+  };
+
+
+  // const formatSpots = () => {
+  //   if (props.spots === 0) {
+  //     return "no spots remaining";
+  //   } else if (props.spots === 1) {
+  //     return "1 spot remaining";
+  //   } else {
+  //     return props.spots + " spots remaining";
+  //   }
+  // };
 
 
   return (
@@ -17,7 +35,7 @@ export default function DayListItem(props) {
       className={dayClass}
     >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
