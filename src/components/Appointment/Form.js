00b4f8576f4f363e64eebc0c0077e780b1029e10
samjanderson <sam.andersonnn@live.ca>
2021-02-16 21:12:09 -0800

    fix bug in form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
-export default function Form({onCancel,...props}) {
+export default function Form(props) {
  console.log('props', props)
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
@@ -14,7 +14,7 @@ export default function Form({onCancel,...props}) {
 
   function cancel() {
     reset()
-    onCancel()
+    props.onCancel()
   }
 
   return (
@@ -24,9 +24,9 @@ export default function Form({onCancel,...props}) {
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
+            value={name}
             type="text"
             placeholder="Enter Student Name"
-            value={name}
             onChange={(event) => setName(event.target.value)}
           /*
             This must be a controlled component
@@ -42,9 +42,11 @@ export default function Form({onCancel,...props}) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel} >Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
         </section>
       </section>
     </main>
   );
 }
+
+{/* <Button confirm onClick={props.onSave}>Save</Button> */}
