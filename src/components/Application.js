7cddf3ef5a85d96a1deef98f96d7da772c9256fb
samjanderson <sam.andersonnn@live.ca>
2021-02-17 16:03:02 -0800

    complete retrieving appts

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,54 +4,54 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
+import { getAppointmentsForDay } from "../helpers/selectors";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  // },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "James Bond",
-      interviewer: {
-        id: 3,
-        name: "Gary",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 4,
-        name: "Dev Ed",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  }
-];
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 3,
+//     time: "2pm",
+//   },
+//   {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "James Bond",
+//       interviewer: {
+//         id: 3,
+//         name: "Gary",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 5,
+//     time: "4pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 4,
+//         name: "Dev Ed",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   }
+// ];
 
 // const days = [
 //   {
@@ -75,22 +75,29 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    // you may put the line below, but will have to remove/comment hardcoded appointments variable
-    // appointments: {}
+    appointments: {}
   });
+
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }))
+  // const setDays = days => setState(prev => ({ ...prev, days }))
 
   useEffect(() => {
-    axios.get("/api/days").then((response) => {
-      // console.log('responsedata', response.data);
-      setDays(response.data)
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments")
+    ]).then((all) => {
+      // console.log(all[0].data); first
+      // console.log(all[1].data); second
+      // const [first, second] = all;
+      // console.log(first, second);
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }));
     });
+
   }, []);
 
-
-
-  // console.log(day);
 
   return (
     <main className="layout">
@@ -105,7 +112,7 @@ export default function Application(props) {
           <DayList
             days={state.days}
             day={state.day}
-            setDay={setDay} 
+            setDay={setDay}
           />
         </nav>
         <img
@@ -115,7 +122,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appointments.map((appointment) => {
+        {dailyAppointments.map((appointment) => {
           return (
             <Appointment
               key={appointment.id}
